3c12963eff7452828cfb01bd7c796e8f4a8ddbf2
Almas Baigabylov <almasbg1@gmail.com>
2020-03-15 12:28:42 -0500

    Day4.example task

--- Day4/Day4.exampletask.py
+++ Day4/Day4.exampletask.py
@@ -2,3 +2,13 @@
 # bucket1 = ["apple", "banana", "cherry", "peach", "carrot", "plum", "grape"]
 # bucket2 = ["apple", "mango", "carrot", "cabbage", "kiwi", "peach"]
 # Please identify same fruits/vegetables and add them to one new list
+
+bucket1 = ["apple", "banana", "cherry", "peach", "carrot", "plum", "grape"]
+bucket2 = ["apple", "mango", "carrot", "cabbage", "kiwi", "peach"]
+sameitems = []
+
+for itema in bucket1:
+    for itemb in bucket2:
+        if itema == itemb:
+            sameitems.append(itema)
+print(sameitems)
