06a7e8aac6bc084e9bf7e6868d735d1e11942491
Almas Baigabylov <almasbg1@gmail.com>
2020-03-15 12:25:01 -0500

    adding homework folder

--- Day4/Day4.exampletask.py
+++ Day4/Day4.exampletask.py
@@ -1,4 +1,4 @@
-# Use for loop.You have two buckets
+# Use nested for loop.You have two buckets
 # bucket1 = ["apple", "banana", "cherry", "peach", "carrot", "plum", "grape"]
 # bucket2 = ["apple", "mango", "carrot", "cabbage", "kiwi", "peach"]
 # Please identify same fruits/vegetables and add them to one new list
