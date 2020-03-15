880e4ac9e35f91b47306031f75631f4c51273d27
DinaraCentos <dinaratkyzy@gmail.com>
2020-03-15 14:00:53 -0500

    adding name/age file

---
+++ Dina/age name.py
@@ -0,0 +1,17 @@
+name1= input("Please enter your name: ")
+age1 = input("Please enter your age:")
+name2= input("Please enter your name: ")
+age2 = input("Please enter your age:")
+if age1 > age2:
+    print (name1 + " "  + age1)
+elif age2 > age1:
+    print (name2 + " " + age2)
+elif age1 == age2:
+    print (name1 + " "  + name2 + "are the same age")
+
+
+
+
+
+
+
