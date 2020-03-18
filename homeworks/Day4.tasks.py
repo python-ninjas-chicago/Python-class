b4728c777fb9b94075777452b2d47530f83a4b81
Merim Abdyldaeva <support@servercentral.com>
2020-03-18 15:36:20 -0500

    Adding 2 homework

---
+++ homeworks/Day4.tasks.py
@@ -0,0 +1,21 @@
+###Day4/Task2
+total_list = [ ]
+while True:
+    input_list = input("Please enter your item: ")
+    if input_list == "exit":
+           break
+    total_list.append(input_list)
+print(total_list)
+
+
+###Day4/Task1
+
+listA = []
+listB = []
+for i in range(1, 52):
+    if i % 2 == 0:
+        listA.append(i)
+    if i % 2 != 0:
+        listB.append(i)
+print(listA)
+print(listB)
