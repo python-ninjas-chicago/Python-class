452f1cf413beb5875ab97cf67f83988bcc190a3c
Almas Baigabylov <almasbg1@gmail.com>
2020-03-29 10:50:00 -0500

    adding file

--- Day8/Day8.1-ClassReview.py
+++ Day8/Day8.1-ClassReview.py
@@ -21,7 +21,23 @@ print("###########################################")
 # Create a class for a fruits so it will have weight, color and days old. Make sure you have a fucntion which
 # returns information about fruit. And make sure you when you add quantity of apple it will calculate total size of
 # whole bag.
-#
 
+class fruits:
+    def __init__(self, name, weight, old_days, color):
+        self.name = name
+        self.weight = weight
+        self.old_days = old_days
+        self.color = color
+
+    def display(self):
+        return f"{self.name} is {self.color}, weight is {self.weight}lb and {self.old_days} days old."
+
+    def calculate_total_weight(self, count):
+        return f"In my bag {count} {self.name}s and total weight is {self.weight * count}lb"
+
+
+f1 = fruits("apple", 2, 4, "red")
+print(f1.display())
+print(f1.calculate_total_weight(10))
 
 
