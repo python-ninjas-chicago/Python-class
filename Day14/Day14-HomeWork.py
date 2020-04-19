ec9bc0ddf2b75df39e7c51db8394f3d11b7e9097
Almas Baigabylov <almasbg1@gmail.com>
2020-04-19 12:09:59 -0500

    adding day 14

---
+++ Day14/Day14-HomeWork.py
@@ -0,0 +1,12 @@
+# Guess the word
+# Create a list 20 fruits and vegg: apple banana carrot pear avocado mango .......
+# randomly select any fruit from list and let the person to guess it by entering letters:
+
+#enter whole word or letter: t
+#Sorry letter not in the word
+#enter whole word or letter: p
+#Letter p exists in the word
+#enter whole word or letter: r
+#Letter r exists in the word
+#enter whole word or letter: pear
+#Yahoo, you got it! It's pear!
