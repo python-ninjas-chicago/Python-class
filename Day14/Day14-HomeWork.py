759d1e7da24513a4bd9d852193b5e3131ae832f8
Almas Baigabylov <almasbg1@gmail.com>
2020-04-25 10:52:44 -0500

    adding day14 homework

--- Day14/Day14-HomeWork.py
+++ Day14/Day14-HomeWork.py
@@ -10,3 +10,38 @@
 #Letter r exists in the word
 #enter whole word or letter: pear
 #Yahoo, you got it! It's pear!
+
+import random
+random_list = ["apple", "peach", "mango", "banana", "grapes", "strawberry", "raspberry", "orange", "pear", "tomato",
+               "carrot", "onion", "potato", "cucumber", "pumpkin", "cabbage", "garlic", "watermelon", "apricot",
+               "avocado"]
+secret_word = random.choice(random_list)
+count = 0
+right_guessed = []
+while True:
+    guess = input("Please enter word or a single letter:")
+    count += 1
+    if guess == secret_word:
+        print(f"Hey you did good job, {secret_word} is right word.")
+        break
+    elif guess in secret_word:
+        right_guessed.append(guess)
+        print(f"{guess} is in the word!")
+    else:
+        print("Try another letter or word")
+    print(f"Letters in words:{right_guessed}, number of guesses: {count}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
