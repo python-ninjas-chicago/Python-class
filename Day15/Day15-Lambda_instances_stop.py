7ec26d7db33249c5f08429d23fecf73cba175c7d
Almas Baigabylov <almasbg1@gmail.com>
2020-04-25 13:09:33 -0500

    adding day15

--- Day15/Day15-Lambda_instances_stop.py
+++ Day15/Day15-Lambda_instances_stop.py
@@ -1,14 +1,17 @@
-import json
 import boto3
 
 ec2 = boto3.client('ec2')
 
+
 def lambda_handler(event, context):
     instances_list = []
     ec2_response = ec2.describe_instances(DryRun=False, MaxResults=123)
-    #print(ec2_response)
+    # print(ec2_response)
     for instance in ec2_response['Reservations']:
         instances_list.append(instance['Instances'][0]['InstanceId'])
     print(instances_list)
-    response = ec2.stop_instances(InstanceIds=instances_list)
-    print(response)
+    try:
+        response = ec2.stop_instances(InstanceIds=instances_list)
+        print(response)
+    except:
+        print("no instances to stop")
